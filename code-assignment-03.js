0c8420bee4e1d7c4478d14c149f0ef33c66879d0
kkappe2 <35344043+kkappe2@users.noreply.github.com>
2018-02-23 23:23:02 -0500

    Add files via upload
    Adding Code Assignment 3

---
+++ code-assignment-03.js
@@ -0,0 +1,15 @@
+let assignmentNumber = 3
+let assignmentTitle = 'Introduction to JavaScript - Variables and the console'
+let assignmentSuccess = true
+let assignmentName = ['assignment01', 'assignment02', 'assignment03']
+
+console.log(assignmentNumber)
+console.log(assignmentTitle)
+console.log(assignmentSuccess)
+console.log(assignmentName)
+
+console.log("")
+console.log('Current Assignment Number: ' + assignmentNumber)
+console.log('Assignment Title: ' + assignmentTitle)
+console.log('Did I pass? ' + assignmentSuccess)
+console.log('Assignments Completed: ' + assignmentName)
