56ac1c7be4488e213711c503e61349e69ddce358
kkappe2 <Briney.Wesley@gmail.com>
2018-03-21 20:47:22 -0400

    Code Assignment 06a
    Adding Code Assignment 06a html and js files

---
+++ code-assignment-06a.js
@@ -0,0 +1,10 @@
+let university = 'LSU';
+
+function describeUniversity (myUniversity) {
+  let description = myUniversity + ' has a beautiful campus.';
+
+  return description;
+};
+
+let universityDescription = describeUniversity(university);
+console.log(universityDescription);
