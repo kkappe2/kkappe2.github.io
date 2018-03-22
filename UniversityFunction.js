32e27cd6b44e4531e62cd26658a5b6773bea83c1
kkappe2 <Briney.Wesley@gmail.com>
2018-03-21 21:18:57 -0400

    Code Assignment 7a
    Adding Code Assignment 7a html, js and function.js files.

---
+++ UniversityFunction.js
@@ -0,0 +1,10 @@
+//let university = 'LSU';
+
+function describeUniversity (myUniversity) {
+  let description = myUniversity + ' has a beautiful campus.';
+
+  console.log(description);
+};
+
+//let universityDescription = describeUniversity(university);
+//console.log(universityDescription);
---
+++ code-assignment-07a.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    describeUniversity('LSU');
+});
